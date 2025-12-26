ed2260fa2530d3b7d621342b15183134089c64ba
Raphael Licha <raphael.licha2@gmail.com>
2025-12-26 11:13:21 +0100

    Add a starter Spring Boot backend to the project

---
+++ paideia_backend/src/main/java/io/paideia/backend/PaideiaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.paideia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaideiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaideiaBackendApplication.class, args);
+	}
+
+}
---
+++ paideia_backend/src/test/java/io/paideia/backend/PaideiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.paideia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaideiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
