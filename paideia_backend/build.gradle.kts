91cf4a5570bb37ce082753a785f7647805be44c4
Raphael Licha <raphael.licha2@gmail.com>
2025-12-29 11:14:55 +0100

    Fix Gradle project name

--- paideia_backend/build.gradle.kts
+++ paideia_backend/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.example"
+group = "io.paideia"
 version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
+description = "Collaborative tech reading platform"
 
 java {
 	toolchain {
@@ -23,8 +23,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
